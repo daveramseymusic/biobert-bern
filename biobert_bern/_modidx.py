9d48d287a3199da8f0ae588e79d557c3a6434ccb
daveramseymusic <daveramseymusic@gmailcom>
2022-11-14 19:57:43 +0000

    fix exports and cross fingers

--- biobert_bern/_modidx.py
+++ biobert_bern/_modidx.py
@@ -6,4 +6,8 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/daveramseymusic/biobert-bern',
                 'lib_path': 'biobert_bern'},
   'syms': { 'biobert_bern.biobertApi': { 'biobert_bern.biobertApi.create_text_block': ( 'biobertapi.html#create_text_block',
-                                                                                        'biobert_bern/biobertApi.py')}}}
+                                                                                        'biobert_bern/biobertApi.py'),
+                                         'biobert_bern.biobertApi.get_comment_spans_textblock': ( 'biobertapi.html#get_comment_spans_textblock',
+                                                                                                  'biobert_bern/biobertApi.py'),
+                                         'biobert_bern.biobertApi.query_plain': ( 'biobertapi.html#query_plain',
+                                                                                  'biobert_bern/biobertApi.py')}}}
